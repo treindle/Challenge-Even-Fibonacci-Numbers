5a063096bd3c137de78ea42f71471eb885e3e5d2
Teresa Ann Nededog <VIRUS@Teresas-MacBook-Air.local>
2015-01-15 11:40:28 -1000

    The Struggle is real

--- challenge_2.js
+++ challenge_2.js
@@ -1,6 +1,27 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
+  var gold= 1.61815754;
+  var num = 
+  ((((gold^n)-(1-gold))^n)/Math.sqrt(5))-n;
+  //
+
+  //
   // do your work here
+ 
+ // var num = ((n-1) + (n-2))*2;
+ //Math.sqrt();
+
+  for (var i = 0; i < n; i++) {
   
+    if (num%2){
+      sum += i;
+      return sum;
+    }  
+  }
+console.log(sum);
   return sum;
 };
+
+
+
+
